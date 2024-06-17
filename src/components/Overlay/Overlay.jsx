17f79d03a80d4bc04a0b40f7c792e1119d3df7e8
Zack Sigel <zack.sigel@gmail.com>
2024-06-17 13:20:08 -0400

    changes "demo" to "testing"

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -11,7 +11,7 @@ const Overlay = ({ onStart }) => {
         <div className='overlay-content-instructions instructions-header'>Race against the clock to guess as many words as you can.</div>
         <div className='overlay-content-instructions'>Longer words are worth more points, but every five seconds a letter will be removed from the board.</div>
         <button className="start-button" onClick={onStart}>Play</button>
-        <div className='overlay-content-instructions demo-note'>This is a demo version with three words hardcoded into the frontend. The game will randomly choose one of the words when you refresh the page.</div>
+        <div className='overlay-content-instructions demo-note'>This is a testing version with three words hardcoded into the frontend. The game will randomly choose one of the words when you refresh the page.</div>
       </div>
     </div>
   );
